2b8948dce18c5d280c1c52c379415e876b1c651a
Ryan Thomas <rthomas@atlassian.com>
2014-04-30 06:58:43 +1000

    Added in some of the attributes and all of the stack map frames, exception info and variable info.

--- src/Class/Types.hs
+++ src/Class/Types.hs
@@ -21,11 +21,16 @@ data ConstantPool = ConstantPool {
   } deriving (Show)
 
 data ClassInfo = ClassInfo {
-  accessFlags :: Word16,
+  classAccessFlags :: Word16,
   thisClass :: Word16,
   superClass :: Word16,
   interfacesCount :: Word16,
   interfaces :: ByteString
+  } deriving (Show)
+
+data Fields = Fields {
+  fieldCount :: Word16,
+  fieldInfo :: [FieldInfo]
   } deriving (Show)
 
 data ConstantPoolInfo = ConstantClass {
@@ -93,4 +98,91 @@ data ConstantPoolInfo = ConstantClass {
   tag :: Word8,
   bootstrapMethodAttrIndex :: Word16,
   nameAndTypeIndex :: Word16
+  } deriving (Show)
+
+data FieldInfo = FieldInfo {
+  fieldAccessFlags :: Word16,
+  fieldNameIndex :: Word16,
+  fieldDescriptorIndex :: Word16,
+  fieldAttributesCount :: Word16,
+  fieldAttributeInfo :: [AttributeInfo]
+  } deriving (Show)
+
+data AttributeInfo = ConstantValueAttribute {
+  attributeNameIndex :: Word16,
+  attributeLength :: Word32,
+  constantValueIndex :: Word16
+  } | CodeAttribute {
+  attributeNameIndex :: Word16,
+  attributeLength :: Word32,
+  maxStack :: Word16,
+  maxLocals :: Word16,
+  codeLength :: Word32,
+  code :: ByteString,
+  exceptionTableLength :: Word16,
+  exceptionTable :: [ExceptionTableEntry],
+  attributesCount :: Word16,
+  attributeInfo :: [AttributeInfo]
+  } | StackMapTableAttribute {
+  attributeNameIndex :: Word16,
+  attributeLength :: Word32,
+  numberOfEntries :: Word16,
+  entries :: [StackMapFrame]
+  } deriving (Show)
+
+data ExceptionTableEntry = ExceptionTableEntry {
+  startPc :: Word16,
+  endPc :: Word16,
+  handlerPc :: Word16,
+  catchType :: Word16
+  } deriving (Show)
+
+data StackMapFrame = SameFrame {
+  frameType :: Word8 -- 0-63
+  } | SameLocals1StackItemFrame {
+  frameType :: Word8, -- 64-127
+  stack :: VerificationTypeInfo
+  } | SameLocals1StackItemFrameExtended {
+  frameType :: Word8, -- 247
+  offsetDelta :: Word16,
+  stack :: VerificationTypeInfo
+  } | ChopFrame {
+  frameType :: Word8, -- 248-250
+  offsetDelta :: Word16
+  } | SameFrameExtended {
+  frameType :: Word8, -- 251
+  offsetDelta :: Word16
+  } | AppendFrame {
+  frameType :: Word8, -- 252-254
+  offsetDelta :: Word16,
+  locals :: [VerificationTypeInfo] -- length: frameType - 251
+  } | FullFrame {
+  frameType :: Word8, -- 255
+  offsetDelta :: Word16,
+  numberOfLocals :: Word16,
+  locals :: [VerificationTypeInfo],
+  numberOfStackItems :: Word16,
+  stackItems :: [VerificationTypeInfo]
+  } deriving (Show)
+
+data VerificationTypeInfo = TopVariableInfo {
+  infoTag :: Word8 -- 0
+  } | IntegerVariableInfo {
+  infoTag :: Word8 -- 1
+  } | FloatVariableInfo {
+  infoTag :: Word8 -- 2
+  } | LongVariableInfo {
+  infoTag :: Word8 -- 4
+  } | DoubleVariableInfo {
+  infoTag :: Word8 -- 3
+  } | NullVariableInfo {
+  infoTag :: Word8 -- 5
+  } | UninitializedThisVariableInfo {
+  infoTag :: Word8 -- 6
+  } | ObjectVariableInfo {
+  infoTag :: Word8, -- 7
+  cPoolIndex :: Word16
+  } | UninitializedVariableInfo {
+  infoTag :: Word8, -- 8
+  offset :: Word16
   } deriving (Show)
